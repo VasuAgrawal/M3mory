25ed351bf1cb1337ed1abe27e2a0ebb2691cba72
Vasu Agrawal <vasu_agrawal@yahoo.com>
2015-11-20 08:52:26 -0500

    Image browsing!

--- pull_data.py
+++ pull_data.py
@@ -1,5 +1,9 @@
 import json
 import urllib2
+import fuckit
+import base64
+import numpy as np
+import cv2
 
 keys = ""
 with open("keys.txt") as f:
@@ -7,7 +11,7 @@ with open("keys.txt") as f:
 
 keys = keys.splitlines()
 
-ckey = keys[0]
+ckey = keys[1]
 
 history = 50
 page = "feed"
@@ -49,15 +53,26 @@ def add_to_dict(d):
 def get(URL):
     response = urllib2.urlopen(URL % ckey).read()
     j = json.loads(response)
-    add_to_dict(j["data"])
+    add_to_dict(j.get("data", []))
 
 def getImage():
     for key in data_dict:
         if "object_id" in data_dict[key].__dict__:
             URL = imageURL % (data_dict[key].object_id, ckey)
-            resp = urllib2.urlopen(URL).read()
-            j = json.loads(resp)
-            print j
+            src = ""
+            with fuckit:
+                resp = urllib2.urlopen(URL).read()
+                j = json.loads(resp)
+                src = j["source"]
+                if ".jpg" in src:
+                    data_dict[key].source = src
+                    f = urllib2.urlopen(src).read()
+                    arr = np.asarray(bytearray(f), dtype=np.uint8)
+                    img = cv2.imdecode(arr, -1)
+                    cv2.imshow("ha ha ha!", img)
+                    if cv2.waitKey() & 0xFF == 27:
+                        cv2.destroyAllWindows()
+
 
 def main():
     get(postURL)
@@ -65,7 +80,10 @@ def main():
     get(likeURL)
     getImage()
 
+    A = "409102435841571"
+
     times = [(data_dict[k].created_time,k) for k in data_dict]
     for t,k in sorted(times)[::-1]:
-        print data_dict[k]
+        # print data_dict[k] if data_dict[k].__dict__.get("object_id") == A else ""
+        print data_dict[k] 
 main()
